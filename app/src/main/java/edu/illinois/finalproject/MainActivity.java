3ab4f6c958fac51c419851aac321af5dbfdcbbd4
lian367 <lianwalsh@gmail.com>
2017-11-15 16:56:55 -0600

    Imported needed libraries for firebase and connected app to firebase with the google-services.json file

--- app/src/main/java/edu/illinois/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/finalproject/MainActivity.java
@@ -3,11 +3,14 @@ package edu.illinois.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
 
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
