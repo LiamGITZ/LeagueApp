9f748df3d1c0a0538dedfa07bdae0251706c913e
lian367 <lianwalsh@gmail.com>
2017-12-04 12:50:32 -0600

    Created summoner spell selection highlighting the currently selected spells

--- app/src/main/java/edu/illinois/finalproject/PlayerGuides/CreateGuide.java
+++ app/src/main/java/edu/illinois/finalproject/PlayerGuides/CreateGuide.java
@@ -1,26 +1,21 @@
 package edu.illinois.finalproject.PlayerGuides;
 
-import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.ResultCodes;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.illinois.finalproject.R;
 
 public class CreateGuide extends AppCompatActivity {
+  private int currentSummonerArrayPos = 0;
+  private String[] summonersChosen = new String[2];
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +24,193 @@ public class CreateGuide extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    
+    // summoner selecting
+    final ImageView smite = findViewById(R.id.smiteSelect);
+    final ImageView flash = findViewById(R.id.flashSelect);
+    final ImageView heal = findViewById(R.id.healSelect);
+    final ImageView exhaust = findViewById(R.id.exhuastSelect);
+    final ImageView teleport = findViewById(R.id.teleportSelect);
+    final ImageView ignite = findViewById(R.id.igniteSelect);
+    final ImageView cleanse = findViewById(R.id.cleanseSelect);
+    final ImageView ghost = findViewById(R.id.ghostSelect);
+    final ImageView barrier = findViewById(R.id.barrierSelect);
+    final Map<String, ImageView> summonerMap = new HashMap<String, ImageView>();
+    summonerMap.put("Smite", smite);
+    summonerMap.put("Flash", flash);
+    summonerMap.put("Heal", heal);
+    summonerMap.put("Exhaust", exhaust);
+    summonerMap.put("Teleport", teleport);
+    summonerMap.put("Ignite", ignite);
+    summonerMap.put("Cleanse", cleanse);
+    summonerMap.put("Ghost", ghost);
+    summonerMap.put("Barrier", barrier);
+
+    barrier.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        barrier.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Barrier";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    ghost.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        ghost.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Ghost";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    cleanse.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        cleanse.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Cleanse";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    ignite.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        ignite.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Ignite";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    teleport.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        teleport.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Teleport";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    exhaust.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        exhaust.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Barrier";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    heal.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        heal.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Heal";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    flash.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        flash.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Flash";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
+
+    smite.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Drawable highlight = getResources().getDrawable(R.drawable.hightlight);
+        if (summonersChosen[currentSummonerArrayPos] != null) {
+          ImageView replace = summonerMap.get(summonersChosen[currentSummonerArrayPos]);
+          replace.setBackground(null);
+        }
+        smite.setBackground(highlight);
+        summonersChosen[currentSummonerArrayPos] = "Smite";
+        if (currentSummonerArrayPos == 0) {
+          currentSummonerArrayPos = 1;
+        } else {
+          currentSummonerArrayPos = 0;
+        }
+      }
+    });
 
 
   }
 
 
-  }
+}
 
 
